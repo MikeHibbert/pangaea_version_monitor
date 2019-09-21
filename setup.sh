23da3e4cbb3c0d9c5e47c78e50e0d6130dca0018
Mike Hibbert <hibbert.michael@gmail.com>
2019-09-21 19:28:12 +0100

    added node supervisor config

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ pip install --yes boto3 arrow
 sudo apt-get install supervisor
 
 echo "Installing configuration files ..."
+mkdir `pwd`/logs
 echo "BASE_DIR = '`pwd`'" >> `pwd`/pangaea_version_monitor/local_settings.py
 sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea_version_monitor/conf/pangaea_version_monitor.conf" > /etc/supervisor/conf.d/pangaea_version_monitor.conf
+sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea_version_monitor/conf/pangaea_node.conf" > /etc/supervisor/conf.d/pangaea_node.conf
 
-sudo supervisorctrl reload
+sudo supervisorctl reload
